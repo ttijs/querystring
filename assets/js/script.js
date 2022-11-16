10002f89782f6199bc10589526e00e40c2219b34
Thomas Tijs <ttijs@glu.nl>
2022-11-16 15:09:21 +0100

    update met localStorage erbij

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-let prodvalue = params.prod; // "some_value"
-let prijsvalue = params.prijs;
 
+// de waarde van "een_key" ophalen in bijv. "https://example.com/?een_key=dit-is-de-waarde"
+// let waarde = params.een_key;
 
-//alert(prodvalue)
-//console.log(prodvalue)
+let prodvalue = params.prod; // de waarde van de de parameter 'prod'
+let prijsvalue = params.prijs; // de waarde van de de parameter 'prijs'
 
-let pd = document.getElementById('productinfo');
-pd.innerHTML = prodvalue;
+// opslaan in localstorage. Kunnen we het op een andere pagina weer opvragen
+localStorage.setItem('productwaarde', prodvalue);
+localStorage.setItem('productprijs', prijsvalue);
 
-let pprijs = document.getElementById('productprijs');
-pprijs.innerHTML = prijsvalue;
+// div zoeken waar de div is waar we de waarde in beeld willen brengen
+let proddiv = document.getElementById('productinfo');
+let prijsdiv = document.getElementById('productprijs');
+
+// de waarde in beeld brengen met innerHTML
+proddiv.innerHTML = prodvalue;
+prijsdiv.innerHTML = prijsvalue;
+
